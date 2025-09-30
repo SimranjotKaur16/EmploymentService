135eaca694baab11136cf7481fb6cbe89b49af88
Simranjot Kaur <kaursimranjot100@gmail.com>
2025-09-30 10:50:57 +0000

    created DepartmentRepository

---
+++ src/main/java/com/example/sjks/EmploymentService/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.example.sjks.EmploymentService.repository;
+
+import com.example.sjks.EmploymentService.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByName(String name);
+    
+}
